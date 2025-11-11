7b03cf6a90e480945aacaf9292e880d5e44c4aec
11249a047-droid <11249a047@kanchiuniv.ac.in>
2025-11-10 22:49:50 -0800

    Add files via upload

---
+++ A047 unit 2/A047#10.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class math
+{
+    public:
+    int add(int a,int b)
+    {
+        return a+b;    
+    }
+    double add(double a,double b)
+    {
+        return a+b;
+    }
+};
+int main()
+{
+    math m;
+    cout<<"sum of 2 int:"<<m.add(5,10)<<endl;
+    cout<<"sum of 2 double:"<<m.add(2.3,5.7)<<endl;
+    return 0;
+}    
---
+++ A047 unit 2/A047#9.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Demo
+{
+    int x;
+    public:
+    Demo(int a)
+    {
+        x=a;
+        cout<<"constructorcalled,x="<<x<<endl;
+    }
+    Demo()
+    {
+    cout<<"destructor called for object with x="<<x<<endl;
+    }
+};
+int main()
+{
+   Demo d1(5),d2(10),d3(15);
+   cout<<"objects are going out of scope..."<<endl;
+   return 0; 
+}       
+    
+    
